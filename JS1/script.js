b964837af1a263a4aeb380e1bfcbfecbcca3495e
DavidGuillenGarcia <dguillengar@gmail.com>
2025-02-20 12:53:06 +0100

    Ejercicio Completado

--- JS1/script.js
+++ JS1/script.js
@@ -1,10 +1,24 @@
 const estudiante = {
-  nombre: 'Ana',
+  nombre: "Ana",
   edad: 20,
-  ciudad: 'Madrid',
+  ciudad: "Madrid",
   calificaciones: {
     matematicas: 85,
     historia: 90,
     ingles: 75,
   },
 };
+
+const estudiante1 = { ...estudiante };
+console.log(estudiante1);
+
+estudiante1.ciudad = "Barcelona";
+const programacion = 95;
+
+const calificacionesNuevas = {
+  ...estudiante.calificaciones,
+  programacion,
+};
+
+const { nombre, edad, ciudad } = estudiante1;
+console.log(nombre, edad, ciudad, calificacionesNuevas);
