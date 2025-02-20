faccc916510bf88616cdf9233479e93d0fb37d43
DavidGuillenGarcia <dguillengar@gmail.com>
2025-02-20 13:44:03 +0100

    sumEveryOther

--- JS1/script.js
+++ JS1/script.js
@@ -13,12 +13,30 @@ const estudiante1 = { ...estudiante };
 console.log(estudiante1);
 
 estudiante1.ciudad = "Barcelona";
-const programacion = 95;
 
 const calificacionesNuevas = {
   ...estudiante.calificaciones,
-  programacion,
+  programacion: 95,
 };
 
 const { nombre, edad, ciudad } = estudiante1;
 console.log(nombre, edad, ciudad, calificacionesNuevas);
+
+function sumEveryOther(...numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+  return total;
+}
+
+function getSum(total, num) {
+  return total + num;
+}
+
+function sumEveryOther2(...numbers) {
+  const suma = [...numbers].reduce(getSum, 0);
+  return suma;
+}
+
+console.log(sumEveryOther2(1, 2, 3, 4, 5));
