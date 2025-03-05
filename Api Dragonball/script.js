46615076143ea610c4c039b416d955dd6eaed3f2
DavidGuillenGarcia <dguillengar@gmail.com>
2025-03-05 23:33:02 +0100

    Filter and Planets

--- Api Dragonball/script.js
+++ Api Dragonball/script.js
@@ -3,6 +3,11 @@ window.onload = () => {
   const searchbar = document.getElementById("searchbar");
   const previous = document.getElementById("previous");
   const next = document.getElementById("next");
+  const gender = document.getElementById("genderSelect");
+  const race = document.getElementById("raceSelect");
+  const affiliation = document.getElementById("affiliationSelect");
+  const myModal = document.getElementById("myModal");
+
   let actualPage = 1;
   previous.style.visibility = "hidden";
 
@@ -14,8 +19,10 @@ window.onload = () => {
     }
   };
 
-  const createCharacter = (name, image, race, ki, description) => {
+  const createCharacter = (id, name, image, race, ki, description) => {
     const newCharacterContainer = document.createElement("div");
+    newCharacterContainer.id = id;
+    newCharacterContainer.style.cursor = "pointer";
     newCharacterContainer.className =
       "characterContainer card card-body m-2 p-3 shadow bg-dark text-white d-flex align-items-center";
     list.appendChild(newCharacterContainer);
@@ -39,6 +46,40 @@ window.onload = () => {
     newCharacterContainer.appendChild(characterRace);
     newCharacterContainer.appendChild(characterKi);
     newCharacterContainer.appendChild(characterDescription);
+    newCharacterContainer.addEventListener("click", () =>
+      getPlanetFromCharacter(id)
+    );
+  };
+
+  const createPlanet = (id, name, image, description) => {
+    const newPlanetModalContainer = document.createElement("div");
+    newPlanetModalContainer.id = id;
+    newPlanetModalContainer.className =
+      "characterContainer modal-dialog modal-dialog-centered bg-dark text-white d-flex align-items-center";
+    list.appendChild(newCharacterContainer);
+    const characterName = document.createElement("h1");
+    const characterImage = document.createElement("img");
+    const characterRace = document.createElement("span");
+    const characterKi = document.createElement("span");
+    const characterDescription = document.createElement("p");
+    characterName.textContent = name;
+    characterImage.src = image;
+    characterName.className = "my-3 p-2";
+    characterImage.className = "images";
+    characterRace.className = "fs-5 mt-2 p-1";
+    characterKi.className = "fs-5 mt-1 p-1";
+    characterDescription.className = " fs-6 p-3";
+    characterRace.textContent = "Race: " + race;
+    characterKi.textContent = "Quantity of ki: " + ki;
+    characterDescription.textContent = description;
+    newCharacterContainer.appendChild(characterName);
+    newCharacterContainer.appendChild(characterImage);
+    newCharacterContainer.appendChild(characterRace);
+    newCharacterContainer.appendChild(characterKi);
+    newCharacterContainer.appendChild(characterDescription);
+    newCharacterContainer.addEventListener("click", () =>
+      getPlanetFromCharacter(id)
+    );
   };
 
   const getAllCharacters = () => {
@@ -53,6 +94,7 @@ window.onload = () => {
         console.log(data);
         for (let i = 0; i < data.items.length; i++) {
           createCharacter(
+            data.items[i].id,
             data.items[i].name,
             data.items[i].image,
             data.items[i].race,
@@ -75,6 +117,7 @@ window.onload = () => {
           console.log(data);
           for (let i = 0; i < data.length; i++) {
             createCharacter(
+              data.items[i].id,
               data[i].name,
               data[i].image,
               data[i].race,
@@ -129,6 +172,7 @@ window.onload = () => {
         console.log(data);
         for (let i = 0; i < data.items.length; i++) {
           createCharacter(
+            data.items[i].id,
             data.items[i].name,
             data.items[i].image,
             data.items[i].race,
@@ -155,6 +199,7 @@ window.onload = () => {
         console.log(data);
         for (let i = 0; i < data.items.length; i++) {
           createCharacter(
+            data.items[i].id,
             data.items[i].name,
             data.items[i].image,
             data.items[i].race,
@@ -168,9 +213,61 @@ window.onload = () => {
       });
   };
 
+  const filterValue = (event) => {
+    console.log(gender.value);
+    clearList();
+    let filter = event.target.getAttribute("name");
+    if (event.target.value == "-") {
+      getAllCharacters();
+    } else {
+      fetch(
+        "https://dragonball-api.com/api/characters?" +
+          filter +
+          "=" +
+          event.target.value
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          for (let i = 0; i < data.length; i++) {
+            createCharacter(
+              data.items[i].id,
+              data[i].name,
+              data[i].image,
+              data[i].race,
+              data[i].ki,
+              data[i].description
+            );
+          }
+        });
+    }
+  };
+
+  const getPlanetFromCharacter = (id) => {
+    fetch("https://dragonball-api.com/api/characters/" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.originPlanet);
+        const planet = data.originPlanet;
+        document.getElementById("planetName").textContent = planet.name;
+        document.getElementById("planetImage").src = planet.image;
+        document.getElementById("planetDescription").textContent =
+          planet.description;
+
+        const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+        myModal.show();
+      });
+  };
+
   next.addEventListener("click", nextPage);
 
   previous.addEventListener("click", previousPage);
+
+  gender.addEventListener("change", filterValue);
+
+  race.addEventListener("change", filterValue);
+
+  affiliation.addEventListener("change", filterValue);
 
   const searchDebounce = debounce(searchCharacter, 900);
 
