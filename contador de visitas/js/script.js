7ae59861d6eae8ecb973b0fe627fa146a17f21e0
DavidGuillenGarcia <dguillengar@gmail.com>
2025-03-11 13:52:50 +0100

    change colors

--- contador de visitas/js/script.js
+++ contador de visitas/js/script.js
@@ -1,6 +1,9 @@
 window.onload = () => {
   const restartBtn = document.getElementById("btnReestablecer");
   const count = document.getElementById("contadorVisitas");
+  let randomColor = "";
+
+  const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
   const addAView = () => {
     if (!localStorage.getItem("views")) {
@@ -19,7 +22,16 @@ window.onload = () => {
     count.innerText = 0;
   };
 
+  const changeToRandomColor = () => {
+    for (let i = 1; i <= 6; i++) {
+      let randomNumber = Math.floor(Math.random() * 16);
+      randomColor = randomColor + colors[randomNumber];
+    }
+    document.body.style.backgroundColor = "#" + randomColor;
+  };
+
   restartBtn.addEventListener("click", deleteViews);
 
   addAView();
+  changeToRandomColor();
 };
