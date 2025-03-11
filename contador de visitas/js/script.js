a9348edbf2f2b93136cd5a88e1fc5a8b6af8a664
DavidGuillenGarcia <dguillengar@gmail.com>
2025-03-11 13:09:35 +0100

    fix

--- contador de visitas/js/script.js
+++ contador de visitas/js/script.js
@@ -2,17 +2,15 @@ window.onload = () => {
   const restartBtn = document.getElementById("btnReestablecer");
   const count = document.getElementById("contadorVisitas");
 
-  if (localStorage.getItem("views") > 0) {
-    count.innerText = localStorage.getItem("views");
-  }
-
   const addAView = () => {
     if (!localStorage.getItem("views")) {
       localStorage.setItem("views", 1);
+      count.innerText = localStorage.getItem("views");
     } else {
       let storedViews = JSON.parse(localStorage.getItem("views"));
       storedViews = storedViews + 1;
       localStorage.setItem("views", storedViews);
+      count.innerText = localStorage.getItem("views");
     }
   };
 
