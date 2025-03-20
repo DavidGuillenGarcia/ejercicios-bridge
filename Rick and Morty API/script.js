84670b267f0cbb1832e7b53e056e60417c0ec39f
DavidGuillenGarcia <dguillengar@gmail.com>
2025-03-20 09:49:54 +0100

    done

---
+++ Rick and Morty API/script.js
@@ -0,0 +1,104 @@
+window.onload = () => {
+  const list = document.getElementById("characterList");
+  const searchbar = document.getElementById("searchbar");
+  let index = 1;
+
+  const fetchCharacters = () => {
+    if (searchbar.value == "") {
+      getAllCharactersDebounce();
+    } else {
+      searchDebounce();
+    }
+  };
+
+  const createCharacter = (id, name, image, origin, status) => {
+    const newCharacterContainer = document.createElement("div");
+    newCharacterContainer.id = id;
+    newCharacterContainer.className =
+      "characterContainer card card-body m-2 p-3 shadow bg-dark text-white d-flex align-items-center";
+    list.appendChild(newCharacterContainer);
+    const characterName = document.createElement("h1");
+    const characterImage = document.createElement("img");
+    const characterOrigin = document.createElement("span");
+    const characterStatus = document.createElement("span");
+    characterName.textContent = name;
+    characterImage.src = image;
+    characterName.className = "my-3 p-2";
+    characterImage.className = "images";
+    characterOrigin.className = "fs-5 mt-2 p-1";
+    characterStatus.className = "fs-5 mt-1 p-1";
+    characterOrigin.textContent = "Origin: " + origin;
+    characterStatus.textContent = "Status of the character: " + status;
+    newCharacterContainer.appendChild(characterName);
+    newCharacterContainer.appendChild(characterImage);
+    newCharacterContainer.appendChild(characterOrigin);
+    newCharacterContainer.appendChild(characterStatus);
+  };
+
+  const getAllCharacters = (index) => {
+    clearList();
+    fetch("https://rickandmortyapi.com/api/character/?page=" + index)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results);
+        for (let i = 0; i < data.results.length; i++) {
+          createCharacter(
+            data.results[i].id,
+            data.results[i].name,
+            data.results[i].image,
+            data.results[i].origin.name,
+            data.results[i].status
+          );
+        }
+      });
+  };
+
+  const searchCharacter = () => {
+    console.log(searchbar.value.length);
+    if (searchbar.value.length >= 3) {
+      clearList();
+      fetch(
+        "https://rickandmortyapi.com/api/character/?name=" + searchbar.value
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          for (let i = 0; i < data.results.length; i++) {
+            createCharacter(
+              data.results[i].id,
+              data.results[i].name,
+              data.results[i].image,
+              data.results[i].origin.name,
+              data.results[i].status
+            );
+          }
+        });
+    } else if (searchbar.value == "") {
+      getAllCharactersDebounce();
+    }
+  };
+
+  const clearList = () => {
+    list.innerHTML = "";
+  };
+
+  searchbar.addEventListener("input", fetchCharacters);
+
+  const debounce = (mainFunction, delay) => {
+    let timer;
+
+    return function (...args) {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        mainFunction(...args);
+      }, delay);
+    };
+  };
+
+  const searchDebounce = debounce(searchCharacter, 900);
+
+  const getAllCharactersDebounce = debounce(getAllCharacters, 100);
+
+  fetchCharacters();
+};
