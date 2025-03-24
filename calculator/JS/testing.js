dadab3c9590f8d12b71e2fa2c34525a0612f3118
DavidGuillenGarcia <dguillengar@gmail.com>
2025-03-24 12:29:10 +0100

    calculator design

--- calculator/JS/testing.js
+++ calculator/JS/testing.js
@@ -2,10 +2,12 @@ const assertEquals = (actual, expected, testName) => {
   if (actual === expected) {
     console.log(`${testName} completed, no errors found.`);
   } else {
-    console.log(
-      `${testName} failed, expected output: ${expected} vs output: ${actual}`
-    );
+    console.log(`${testName} failed`);
+    console.log(`Output: ${actual}`);
+    console.log(`Expected output: ${expected}`);
   }
 };
 
-console.log(assertEquals(sum(2, 3), 6, "Sum test"));
+console.log(assertEquals(sum(2, 3), 6, "Sum test:"));
+
+console.log(assertEquals("17", 17, "String to integer test:"));
