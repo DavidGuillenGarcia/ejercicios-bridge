1b5a45bfe84ea10182befdd4b346b4c540fa29bb
= <=>
2025-03-25 10:26:52 +0100

    basic functions ans styles

--- calculator/JS/functions.js
+++ calculator/JS/functions.js
@@ -7,5 +7,14 @@ const sum = (number1, number2) => {
 };
 
 const subtract = (number1, number2) => {
-  return number1 - number2;
+  return stringToInt(number1) - stringToInt(number2);
 };
+
+const multiply = (number1, number2) => {
+  return stringToInt(number1) * stringToInt(number2);
+}
+
+const divide = (number1, number2) => {
+  return stringToInt(number1) / stringToInt(number2);
+}
+
