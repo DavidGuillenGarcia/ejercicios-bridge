43e0b40e3256afbfa3e77d0b0956154e789b1245
= <=>
2025-03-26 13:41:41 +0100

    refactor

--- calculator/JS/functions.js
+++ calculator/JS/functions.js
@@ -1,34 +1,37 @@
-// let number1 = 1;
-// let number2 = 2;
-// let string = "";
-
 const stringToInt = (string) => {
-    return Number(string);
-  };
-  
-  const sum = (number1, number2) => {
-    return stringToInt(number1) + stringToInt(number2);
-  };
-  
-  const subtract = (number1, number2) => {
-    return stringToInt(number1) - stringToInt(number2);
-  };
-  
-  const multiply = (number1, number2) => {
-    return stringToInt(number1) * stringToInt(number2);
-  }
-  
-  const divide = (number1, number2) => {
-  
-      return stringToInt(number1) / stringToInt(number2);
- }
-  
-  const power = (number1, number2) => {
-    return Math.pow(number1, number2)
-  }
-  
-  const squareRoot = (number1) => {
-    return Math.sqrt(number1);
-  }
+  return Number(string);
+};
 
-module.exports = { stringToInt, sum, subtract, multiply, divide, power, squareRoot};
+const sum = (number1, number2) => {
+  return stringToInt(number1) + stringToInt(number2);
+};
+
+const subtract = (number1, number2) => {
+  return stringToInt(number1) - stringToInt(number2);
+};
+
+const multiply = (number1, number2) => {
+  return stringToInt(number1) * stringToInt(number2);
+};
+
+const divide = (number1, number2) => {
+  return stringToInt(number1) / stringToInt(number2);
+};
+
+const power = (number1, number2) => {
+  return Math.pow(number1, number2);
+};
+
+const squareRoot = (number1) => {
+  return Math.sqrt(number1);
+};
+
+module.exports = {
+  stringToInt,
+  sum,
+  subtract,
+  multiply,
+  divide,
+  power,
+  squareRoot,
+};
