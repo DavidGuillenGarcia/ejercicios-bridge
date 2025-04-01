680894acd31ffe00addbbe4cfec19e4a278e7479
= <=>
2025-04-01 12:44:48 +0200

    sequence

--- simon/JS/script.js
+++ simon/JS/script.js
@@ -2,11 +2,11 @@ const message = document.getElementById("messsage");
 const messageContainer = document.getElementById("message-container");
 const simonButtons = document.querySelectorAll(".simon-btn");
 const playAgainButton = document.getElementById("play-again-btn");
-const playAgainContainer = document.getElementById("play-again-container");
 
 const colors = ["red", "green", "yellow", "blue"];
 let colorsRandomized = [];
 let numberOfColors = 3;
+let count = 0;
 
 const randomizeColors = () => {
   for (let i = 0; i < numberOfColors; i++) {
@@ -17,4 +17,34 @@ const randomizeColors = () => {
   console.log(colorsRandomized);
 };
 
+const checkColor = (event) => {
+  console.log(event.target.id);
+};
+
+async function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function playSequence() {
+  console.log("Mostrando secuencia:", colorsRandomized);
+  const flashDuration = 1300;
+  const pauseDuration = 1300;
+  for (const color of colorsRandomized) {
+    const colorBox = document.getElementById(color);
+    if (colorBox) {
+      colorBox.classList.add("pressed");
+      await wait(flashDuration);
+      colorBox.classList.remove("pressed");
+      await wait(pauseDuration);
+    }
+  }
+}
+
+const addListeners = () => {
+  simonButtons.forEach((btn) => {
+    btn.addEventListener("click", checkColor);
+  });
+  playAgainButton.addEventListener("click", playSequence);
+};
+
 randomizeColors();
+addListeners();
