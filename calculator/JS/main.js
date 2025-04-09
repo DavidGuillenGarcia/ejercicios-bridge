276eca254452ed6e943dae85a34b40030e6821ae
= <=>
2025-04-09 09:49:25 +0200

    fix

--- calculator/JS/main.js
+++ calculator/JS/main.js
@@ -1,14 +1,33 @@
-const {
-  sum,
-  subtract,
-  multiply,
-  divide,
-  power,
-  squareRoot,
-} = require("functions");
-
 const result = document.getElementById("result");
 let currentOperation;
+
+const stringToInt = (string) => {
+  return Number(string);
+};
+
+const sum = (number1, number2) => {
+  return stringToInt(number1) + stringToInt(number2);
+};
+
+const subtract = (number1, number2) => {
+  return stringToInt(number1) - stringToInt(number2);
+};
+
+const multiply = (number1, number2) => {
+  return stringToInt(number1) * stringToInt(number2);
+};
+
+const divide = (number1, number2) => {
+  return stringToInt(number1) / stringToInt(number2);
+};
+
+const power = (number1, number2) => {
+  return Math.pow(number1, number2);
+};
+
+const squareRoot = (number1) => {
+  return Math.sqrt(number1);
+};
 
 const calc = () => {
   if (result.innerText.includes(currentOperation)) {
